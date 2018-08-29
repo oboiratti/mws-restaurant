3b8bf8449b46ef14bf3940a4e72518700d85a2cf
Fred Kumi <ratti10gh@yahoo.com>
2018-08-29 12:41:08 +0000

    fix: use appropriate semantic for breadcrumb

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -178,10 +178,10 @@ createReviewHTML = (review) => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
+  const ul = document.getElementById('bc-ul');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
-  breadcrumb.appendChild(li);
+  ul.appendChild(li)
 }
 
 /**
